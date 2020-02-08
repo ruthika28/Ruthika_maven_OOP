94c8e15446e0cf8a971ddb97ad810c2aeb3ebc30
ruthika28 <33190368+ruthika28@users.noreply.github.com>
2020-02-08 22:38:22 +0530

    Add files via upload

---
+++ gifts/src/main/java/com/ruthika/gifts/app.java
@@ -0,0 +1,7 @@
+package com.ruthika.gifts;
+public class app {
+    public static void main(String[] args)
+    {
+        application app= new application();
+    }
+}
---
+++ gifts/src/main/java/com/ruthika/gifts/application.java
@@ -0,0 +1,20 @@
+package com.ruthika.gifts;
+import com.ruthika.gifts.sweets.*;
+import java.util.ArrayList;
+public class application<Sweet> {
+    private GenC gc;
+    ArrayList<Sweet> sweets;
+    public application()
+    {
+        gc=new GenC();
+    }
+    private void process()
+    {
+        int n= (int) (Math.random()*10);
+        gc.generateGift(n);
+    }
+    public void start()
+    {
+        process();
+    }
+}
